7e0f412d623546c9a09247573fd19e7c6c8d17d3
Yanick Champoux <yanick@babyl.dyndns.org>
2017-01-03 11:31:23 -0500

    test with tap

---
+++ test/json-schema-shorthand.js
@@ -0,0 +1,63 @@
+import shorthand from '../src/json-schema-shorthand';
+const tap = require('tap');
+
+tap.Test.prototype.addAssert('shorthand', 2, function(observed, expected, message, extra) {
+    return this.same( shorthand(observed), expected,  message, extra );
+});
+
+tap.test( 'shortcuts', t => {
+
+    t.shorthand( 'string', { type: 'string' }, 'type as string' );
+
+    t.shorthand( { object: { foo: 'string' } },  
+        { type: 'object', properties: { foo: { type: 'string' } } },
+        'object property'
+    );
+
+    t.shorthand( '#foo', { '$ref': '#foo' }, 'expands #ref' );
+
+    t.shorthand( { object: { foo: {}  } }, 
+        { type: 'object', properties: { foo: { } } },
+        'expands objects' 
+    );
+
+    t.shorthand(
+        { array: [ 'number' ] },
+        { type: 'array', items: [ { type: 'number' } ] },
+        'expands array');
+
+    t.shorthand(
+        { array: 'number' },
+        { type: 'array', items:  { type: 'number' }  },
+        'expands array');
+
+    t.shorthand( { properties: { foo: { required: true } } },
+        { required: [ 'foo' ], properties: { foo: { } } },
+        'expands required' );
+
+    [ 'allOf', 'anyOf', 'oneOf' ].forEach( keyword => {
+            let short = {};
+            short[keyword] = [ 'number' ];
+
+            let expected = {};
+
+            expected[keyword] = [ { type: 'number' } ];
+
+            t.shorthand( short, expected );
+    });
+
+    t.shorthand(
+        { 'definitions': { "foo": 'object' }},
+        { definitions: { foo: { type: 'object' } } },
+        'expands definitions'
+    );
+
+    t.shorthand( 
+        { 'not': 'object' },
+        { not: { type: 'object' } },
+        'expands not' 
+    );
+
+    t.end();
+
+});
