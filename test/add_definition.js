007a7f4e40ca23b9d101a988cd07c005994fd5b2
Yanick Champoux <yanick@babyl.dyndns.org>
2018-02-13 09:54:51 -0500

    rename the test file

--- test/add_definition.js
+++ test/add_definition.js
@@ -12,7 +12,7 @@ tap.test( 'with ::', t => {
 
     let defs = {};
 
-    let foo = defs::add_definition( 'foo', 'number' );
+    let foo = defs::add_definition( 'foo', number() );
 
     t.same( defs, { foo: { type: 'number' } }, "defs updated" );
     t.same( foo, { '$ref': '#/definitions/foo'  }, "foo is a schema ref" );
@@ -25,7 +25,7 @@ tap.test( 'with .bind()', t => {
     let defs = {};
     const add_def = add_definition.bind(defs);
 
-    let foo = add_def( 'foo', 'number' );
+    let foo = add_def( 'foo', number() );
 
     t.same( defs, { foo: { type: 'number' } }, "defs updated" );
     t.same( foo, { '$ref': '#/definitions/foo'  }, "foo is a schema ref" );
