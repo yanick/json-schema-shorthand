c1c045031523baee688facbfa69fb3cf98b26de9
Yanick Champoux <yanick@babyl.dyndns.org>
2020-01-31 15:32:09 -0500

    prettier

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   presets: ["@babel/env"],
-  plugins: [
-  ]
+  plugins: []
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import fp from 'lodash/fp';
-import u from 'updeep';
+import fp from "lodash/fp";
+import u from "updeep";
 
 function groomRange(min, max, min_inc = true, max_inc = true) {
   return {
-    [min_inc ? 'minimum' : 'exclusiveMinimum']: min,
-    [max_inc ? 'maximum' : 'exclusiveMaximum']: max,
+    [min_inc ? "minimum" : "exclusiveMinimum"]: min,
+    [max_inc ? "maximum" : "exclusiveMaximum"]: max
   };
 }
 
@@ -12,59 +12,59 @@ const process_if_has = fp.curry(function(prop, update, obj) {
   return u.if(fp.has(prop), update, obj);
 });
 
-const process_nbrItems = process_if_has('nbrItems', obj => {
-  const {nbrItems} = obj;
+const process_nbrItems = process_if_has("nbrItems", obj => {
+  const { nbrItems } = obj;
   const [minItems, maxItems] = Array.isArray(nbrItems)
     ? nbrItems
     : [nbrItems, nbrItems];
 
-  return fp.flow([u({minItems, maxItems}), u.omit('nbrItems')])(obj);
+  return fp.flow([u({ minItems, maxItems }), u.omit("nbrItems")])(obj);
 });
 
 const process_range = process_if_has(
-  'range',
-  fp.flow([obj => u(groomRange(...obj.range), obj), u.omit(['range'])]),
+  "range",
+  fp.flow([obj => u(groomRange(...obj.range), obj), u.omit(["range"])])
 );
 
 //  expand the shorthand content of `items`
-const expand_items = process_if_has('items', {
+const expand_items = process_if_has("items", {
   items: items =>
-    Array.isArray(items) ? items.map(shorthand) : shorthand(items),
+    Array.isArray(items) ? items.map(shorthand) : shorthand(items)
 });
 
 function groom_required_properties(obj) {
-  if (!obj.hasOwnProperty('properties')) return obj;
+  if (!obj.hasOwnProperty("properties")) return obj;
 
   let required = obj.required || [];
 
   required = required.concat(
-    Object.keys(fp.pickBy('required', obj.properties)),
+    Object.keys(fp.pickBy("required", obj.properties))
   );
   required.sort();
 
   return u({
     required: u.if(required.length, required),
-    properties: u.map(u.omit('required')),
+    properties: u.map(u.omit("required"))
   })(obj);
 }
 
 const process_array = process_if_has(
-  'array',
+  "array",
   fp.flow([
-    u(({array: items}) => ({
-      type: 'array',
-      items,
+    u(({ array: items }) => ({
+      type: "array",
+      items
     })),
-    u.omit(['array']),
-  ]),
+    u.omit(["array"])
+  ])
 );
 
 const process_object = process_if_has(
-  'object',
+  "object",
   fp.flow([
-    u(({object: properties}) => ({type: 'object', properties})),
-    u.omit(['object']),
-  ]),
+    u(({ object: properties }) => ({ type: "object", properties })),
+    u.omit(["object"])
+  ])
 );
 
 const map_shorthand = u.if(fp.identity, u.map(shorthand));
@@ -75,24 +75,24 @@ const expand_shorthands = u({
   properties: map_shorthand,
   anyOf: map_shorthand,
   allOf: map_shorthand,
-  oneOf: map_shorthand,
+  oneOf: map_shorthand
 });
 
 export default function shorthand(obj = {}) {
-  if (typeof obj === 'string') {
+  if (typeof obj === "string") {
     obj =
-      obj[0] === '$'
-        ? {$ref: obj.slice(1)}
-        : obj[0] === '#'
-        ? {$ref: obj}
-        : {type: obj};
+      obj[0] === "$"
+        ? { $ref: obj.slice(1) }
+        : obj[0] === "#"
+        ? { $ref: obj }
+        : { type: obj };
   }
 
-  ['$ref', 'type'].forEach(field => {
+  ["$ref", "type"].forEach(field => {
     if (/!$/.test(obj[field])) {
       obj = u({
         required: true,
-        [field]: v => v.replace(/!$/, ''),
+        [field]: v => v.replace(/!$/, "")
       })(obj);
     }
   });
@@ -104,37 +104,37 @@ export default function shorthand(obj = {}) {
     groom_required_properties,
     expand_items,
     process_nbrItems,
-    process_range,
+    process_range
   ])(obj);
 }
 
 function merge_defs(...defs) {
   return shorthand(
     defs
-      .map(d => (typeof d === 'string' ? {description: d} : d))
-      .reduce((merged, addition) => u(addition)(merged), {}),
+      .map(d => (typeof d === "string" ? { description: d } : d))
+      .reduce((merged, addition) => u(addition)(merged), {})
   );
 }
 
-const simple_def = type => (...options) => merge_defs({type}, ...options);
+const simple_def = type => (...options) => merge_defs({ type }, ...options);
 
-export const number = simple_def('number');
-export const integer = simple_def('integer');
-export const string = simple_def('string');
+export const number = simple_def("number");
+export const integer = simple_def("integer");
+export const string = simple_def("string");
 
 export const array = (items = null, ...options) =>
-  merge_defs({type: 'array', items: u.if(items, items)}, ...options);
+  merge_defs({ type: "array", items: u.if(items, items) }, ...options);
 
 export const object = (properties = null, ...options) =>
   merge_defs(
     {
-      type: 'object',
-      properties: u.if(properties, properties),
+      type: "object",
+      properties: u.if(properties, properties)
     },
-    ...options,
+    ...options
   );
 
 export function add_definition(name, ...schemas) {
   this[name] = merge_defs(...schemas);
-  return {$ref: '#/definitions/' + name};
+  return { $ref: "#/definitions/" + name };
 }
